5128a3d20a9418ffc8956c33da97e105b8d453ce
Robin Moew <robin.marcot1992@gmail.com>
2022-07-25 22:37:24 +0200

    workspace list OK - added single selection mode - saving files as JSON - looking for a way to manage getting data

--- workspace-getter-setter.py
+++ workspace-getter-setter.py
@@ -12,20 +12,21 @@ class Listbox(sg.Listbox):
         e.accept()
 
     def dropEvent(self, e):
-        data = window['LISTBOX'].get_list_values()
+        data = window['PATH_LISTBOX'].get_list_values()
         items = [str(v) for v in e.mimeData().text().strip().split('\n')]
         items = [path.removeprefix('file:///') for path in items]
         items = list(set(items) - set(data))
         data.extend(items)
-        window['LISTBOX'].update(data)
+        window['PATH_LISTBOX'].update(data)
         window.refresh()
 
     def doubleClickEvent(self, e):
-        item = self.QT_ListWidget.itemAt(
-            e.pos().x(), e.pos().y()
-        )
-        if None != item:
-            open_file(item.text())
+        if 'PATH_LISTBOX' == self.Key:
+            item = self.QT_ListWidget.itemAt(
+                e.pos().x(), e.pos().y()
+            )
+            if None != item:
+                open_file(item.text())
 
     def enable_drop(self):
         self.Widget.setAcceptDrops(True)
@@ -37,17 +38,21 @@ class Listbox(sg.Listbox):
         self.Widget.mouseDoubleClickEvent = self.doubleClickEvent
 
 
+def checkWorkspace():
+    pass
+
+
 def search(values):
-    global results
-    window['LISTBOX'].update(values=results)
+    global path_results
+    window['PATH_LISTBOX'].update(values=path_results)
 
     for root, _, files in os.walk(values['PATH']):
         for file in files:
             file = f'{root}\\{file}'.replace('\\', '/')
-            if file not in results:
+            if file not in path_results:
                 if values['TERM'].lower() in file.lower():
-                    results.append(file)
-                    window['LISTBOX'].update(results)
+                    path_results.append(file)
+                    window['PATH_LISTBOX'].update(path_results)
 
 
 def open_file(file_name):
@@ -56,28 +61,34 @@ def open_file(file_name):
 
 
 def open_selected():
-    for f in window['LISTBOX'].get():
+    for f in window['PATH_LISTBOX'].get():
         open_file(f)
 
 
 def open_all():
-    for f in window['LISTBOX'].get_list_values():
+    for f in window['PATH_LISTBOX'].get_list_values():
         open_file(f)
 
 
-def suppr_selected():
-    items = window['LISTBOX'].get_list_values()
-    selectedItems = window['LISTBOX'].get()
+def suppr_selected(listbox):
+    print(listbox)
+    items = window[listbox].get_list_values()
+    selectedItems = window[listbox].get()
     items = list(set(items) - set(selectedItems))
-    window['LISTBOX'].update(items)
+    window[listbox].update(items)
+    listbox = ''
 
 
 def addWorkspace():
+    global workspaces_results
+    window['WORKSPACE_LISTBOX'].update(values=workspaces_results)
     workspace_name = sg.popup_get_text(
         'Nom: ',
         keep_on_top=True
     )
-    workspaces = window['WORKSPACE_LISTBOX'].get_list_values()
+    if workspace_name not in workspaces_results:
+        workspaces_results.append(workspace_name)
+        window['WORKSPACE_LISTBOX'].update(workspaces_results)
 
 
 def setWorkspace(workspace, pathList):
@@ -85,13 +96,14 @@ def setWorkspace(workspace, pathList):
         json.dump(pathList, file, indent=2)
 
 
-def getWorkspace():
-    pass
-    # with open(workspace, 'w') as file:
-    #     json.dump(pathList, file)
+def getWorkspace(workspace):
+    with open(workspace, 'w') as file:
+        print(file)
+        json.dump(file)
 
 
-results = []
+path_results = []
+workspaces_results = []
 sg.change_look_and_feel('LightGreen7')
 layout = [
     [
@@ -105,17 +117,17 @@ layout = [
     [
         sg.Text('Workspaces', size=(11, 1)),
         sg.Button('+', size=(4, 1), key='ADD'),
-        sg.Button('-', size=(4, 1), key='SUPPR_WORKSPACE'),
+        sg.Button('-', size=(4, 1), key='SUPPR_WORKSPACE_LISTBOX'),
         sg.Text('', size=(1, 1)),
         sg.Text('Fichiers', size=(47.5, 1)),
         sg.Button('Ouvrir', size=(10, 1), key='OPEN'),
         sg.Button('Ouvrir tout', size=(10, 1), key='OPEN_ALL'),
-        sg.Button('Supprimer', size=(10, 1), key='SUPPR')
+        sg.Button('Supprimer', size=(10, 1), key='SUPPR_PATH_LISTBOX')
     ],
     [
-        sg.Listbox(values=results, size=(20, 10),
-                   enable_events=True, key='WORKSPACE_LISTBOX'),
-        Listbox(values=results, size=(80, 10),
+        Listbox(values=workspaces_results, select_mode=sg.SELECT_MODE_SINGLE, size=(20, 10),
+                enable_events=True, key='WORKSPACE_LISTBOX'),
+        Listbox(values=path_results, size=(80, 10),
                 enable_events=True, key='PATH_LISTBOX')
     ]
 ]
@@ -130,13 +142,14 @@ window = sg.Window('Work spaces', layout=layout,
                    finalize=True, return_keyboard_events=True)
 window['PATH_LISTBOX'].enable_drop()
 window['PATH_LISTBOX'].enable_double_click()
+window['WORKSPACE_LISTBOX'].enable_double_click()
 
 # main event loop
 while True:
     event, values = window.read()
     if event is None:
         break
-    if event == 'SEARCH' or event == 'special 16777220' and 'TERM' != '' and 'PATH' != '':
+    if event == 'SEARCH' and 'TERM' != '' and 'PATH' != '':
         search(values)
     if event == 'ADD':
         addWorkspace()
@@ -144,5 +157,8 @@ while True:
         open_selected()
     if event == 'OPEN_ALL':
         open_all()
-    if event == 'SUPPR':
-        suppr_selected()
+    if event == 'SUPPR_PATH_LISTBOX' or event == 'SUPPR_WORKSPACE_LISTBOX':
+        suppr_selected(event.removeprefix('SUPPR_'))
+    if event == 'WORKSPACE_LISTBOX':
+        print(window['WORKSPACE_LISTBOX'].get())
+        # getWorkspace(values['WORKSPACE_LISTBOX'])
